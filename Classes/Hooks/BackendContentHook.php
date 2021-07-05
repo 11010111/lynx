bac45a232de356ed2fac7bae549c05480e830216
Konstantin Schneider <46787835+11010111@users.noreply.github.com>
2021-07-05 21:13:37 +0200

    [update]

--- Classes/Hooks/BackendContentHook.php
+++ Classes/Hooks/BackendContentHook.php
@@ -2,8 +2,10 @@
 
 namespace Swe\Lynx\Hooks;
 
+use phpDocumentor\Reflection\Types\Integer;
 use TYPO3\CMS\Backend\View\PageLayoutView;
 use TYPO3\CMS\Backend\View\PageLayoutViewDrawFooterHookInterface;
+use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
 
 /**
  * Class BackendContentHook
@@ -12,6 +14,11 @@ use TYPO3\CMS\Backend\View\PageLayoutViewDrawFooterHookInterface;
 class BackendContentHook implements PageLayoutViewDrawFooterHookInterface
 {
     /**
+     * @var string
+     */
+    private $extensionKey = 'lynx';
+
+    /**
      * @param PageLayoutView $parentObject
      * @param array $info
      * @param array $row
@@ -19,27 +26,49 @@ class BackendContentHook implements PageLayoutViewDrawFooterHookInterface
     public function preProcess(PageLayoutView &$parentObject, &$info, array &$row)
     {
         if ($row['layout'] == 1) {
-            $info[] = '<b>Full Width</b>';
+            $info[] = '<b>' . LocalizationUtility::translate('full_width', $this->extensionKey) . '</b>';
         }
 
         if ($row['frame_class'] != 'default') {
-            $info[] = '<b>Frame Class</b> ' . $row['frame_class'];
+            $info[] = '<b>' . LocalizationUtility::translate('frame_class', $this->extensionKey) . '</b> ' . $row['frame_class'];
         }
 
-        if ($row['frame_class_extra'] != '') {
-            $info[] = '<b>Frame Class Extra</b> ' . $row['frame_class_extra'];
+        if ($row['frame_class_extra']) {
+            $info[] = '<b>' . LocalizationUtility::translate('frame_class_extra', $this->extensionKey) . '</b> ' . $row['frame_class_extra'];
         }
 
-        if ($row['space_left_class'] != '') {
-            $info[] = '<b>Space Left</b> ' . $row['space_left_class'];
+        if ($row['space_left_class']) {
+            $result = null;
+
+            if ((int)$row['space_left_class'] > 0) {
+                $result = (int) $row['space_left_class'];
+            } else {
+                $result = LocalizationUtility::translate(
+                    str_replace('-', '_', $row['space_left_class']), $this->extensionKey
+                );
+            }
+
+            $info[] = '<b>' . LocalizationUtility::translate('space_left', $this->extensionKey) . ' </b>' . $result;
         }
 
-        if ($row['space_right_class'] != '') {
-            $info[] = '<b>Space Right</b> ' . $row['space_right_class'];
+        if ($row['space_right_class']) {
+            $result = null;
+
+            if ((int)$row['space_right_class'] > 0) {
+                $result = (int)$row['space_right_class'];
+            } else {
+                $result = LocalizationUtility::translate(
+                    str_replace('-', '_', $row['space_right_class']), $this->extensionKey
+                );
+            }
+
+            $info[] = '<b>' . LocalizationUtility::translate('space_right', $this->extensionKey) . ' </b>' . $result;
+
         }
 
-        if ($row['no_mobile'] != 0) {
-            $info[] = '<b>No Mobile</b>';
+        if ($row['display_on']) {
+            $info[] = '<b>' . LocalizationUtility::translate('display_on', $this->extensionKey) . ' </b>'
+                . LocalizationUtility::translate('display_on.' . $row['display_on'], $this->extensionKey);
         }
     }
 
