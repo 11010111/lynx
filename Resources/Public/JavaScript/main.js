4e35432341d39019f0e3cecd09a03b9dc5b5dc60
Konstantin Schneider <46787835+11010111@users.noreply.github.com>
2021-07-07 21:48:36 +0200

    [update]

--- Resources/Public/JavaScript/main.js
+++ Resources/Public/JavaScript/main.js
@@ -208,3 +208,34 @@ console.log("WE LOVE TYPO3");
     }
 
 })();
+
+/**
+ * Mobile Menu Click Events
+ */
+(function MobileMenu() {
+
+    let mobileMenu = document.querySelector('.mobile-menu')
+
+    if (mobileMenu) {
+        let parent = mobileMenu.querySelectorAll('.parent')
+
+        parent.forEach(function(par) {
+            if (par.firstElementChild.classList.contains('active')) {
+                par.classList.add('parent-open')
+            }
+
+            par.addEventListener('click', function() {
+                if (!par.classList.contains('parent-open')) {
+                    parent.forEach(function(p) {
+                        p.classList.remove('parent-open')
+                    });
+
+                    par.classList.add('parent-open')
+                } else {
+                    par.classList.toggle('parent-open')
+                }
+            });
+        });
+    }
+
+})();
