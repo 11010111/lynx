59611dc8d7882fec6c0d2e2a48077df75981d19e
k-schneider <k.schneider@s-w-e.com>
2021-07-12 13:58:05 +0200

    [update]

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
             'typo3' => '9.5.0-11.9.99',
             'fluid_styled_content' => '9.5.0-11.9.99',
             'rte_ckeditor' => '9.5.0-11.9.99',
-            'gridelements' => '9.5.0-11.9.99',
+            'gridelements' => '9.5.0-10.9.99',
         ],
         'conflicts' => [
         ],
