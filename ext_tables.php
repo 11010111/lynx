e69b98b8b161cc059b3f8ec9b145f5fc8d07f2b6
Konstantin Schneider <k.schneider@s-w-e.com>
2021-10-16 00:02:54 +0200

    [update]

--- ext_tables.php
+++ ext_tables.php
@@ -1,3 +1,12 @@
 <?php
 
 defined('TYPO3_MODE') || die();
+
+if (!\TYPO3\CMS\Core\Core\Environment::getContext()->isProduction()) {
+    $GLOBALS['TBE_STYLES']['skins']['lynx'] = [
+        'name' => 'Development',
+        'stylesheetDirectories' => [
+            'css' => 'fileadmin/lynx/scss/compiled/backend.css'
+        ]
+    ];
+}
