de02f07877e5d46a9e3f8158e64f733e3342c12a
Konstantin Schneider <k.schneider@s-w-e.com>
2021-10-16 15:48:06 +0200

    [update]

--- ext_tables.php
+++ ext_tables.php
@@ -3,16 +3,6 @@
 defined('TYPO3_MODE') || die();
 
 /**
- * Backend CSS FILES
- */
-$GLOBALS['TBE_STYLES']['skins']['lynx'] = [
-    'name' => 'Backend Styles',
-    'stylesheetDirectories' => [
-        'css' => 'fileadmin/lynx/css/backend/'
-    ]
-];
-
-/**
  * Development Mode CSS FILES
  */
 if (!\TYPO3\CMS\Core\Core\Environment::getContext()->isProduction()) {
