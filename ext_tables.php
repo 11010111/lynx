11123ac7c8a4cf35ce998459fed63118bd223fdd
Konstantin Schneider <k.schneider@s-w-e.com>
2021-10-18 20:22:43 +0200

    [update]

--- ext_tables.php
+++ ext_tables.php
@@ -13,15 +13,3 @@ if (!\TYPO3\CMS\Core\Core\Environment::getContext()->isProduction()) {
         ]
     ];
 }
-
-/*****************************
- * Add Backend configuration *
- ****************************/
-$GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['backend'] = [
-    'backendFavicon' => 'fileadmin/lynx/icons/favicon.ico',
-    'backendLogo' => 'fileadmin/lynx/images/logo_backend.png',
-    'loginFootnote' => 'Smart Web Elements',
-    'loginLogo' => 'fileadmin/lynx/images/logo.png',
-    'loginLogoAlt' => 'Logo'
-
-];
