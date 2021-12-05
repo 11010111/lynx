4e8d0e5b8e80a3964ef87cd2597ced3cd93b08f6
Konstantin Schneider <k.schneider@s-w-e.com>
2021-12-05 16:53:56 +0100

    [update] add classes on replacement

--- Initialisation/Files/js/img-to-svg.js
+++ Initialisation/Files/js/img-to-svg.js
@@ -2,28 +2,31 @@
  * CONVERT IMG TAG TO SVG
  */
 (function ConvertIMGSVG() {
+    let images = document.querySelectorAll('.svg')
 
-    let images = document.querySelectorAll('.svg');
+    if (!images) return
 
-    if (!images) {
-        return
-    }
-
-    for (let i = 0; i < images.length; i += 1) {
-        let extSplit = images[i].src.split('.');
-        let extension = extSplit[extSplit.length - 1].toLowerCase();
+    images.forEach(function(image) {
+        let extSplit = image.src.split('.')
+        let extension = extSplit[extSplit.length - 1].toLowerCase()
 
         if (extension === 'svg') {
-            let xhr = new XMLHttpRequest();
-            xhr.open("GET",images[i].src);
-            xhr.overrideMimeType("image/svg+xml");
+            let xhr = new XMLHttpRequest()
+            xhr.open("GET", image.src)
+            xhr.overrideMimeType("image/svg+xml")
             xhr.addEventListener('load', function() {
-                let svg = xhr.responseXML.documentElement;
-                svg.classList.add('fade-in');
-                images[i].parentElement.replaceChild(svg, images[i]);
-            });
-            xhr.send();
-        }
-    }
+                let svg = xhr.responseXML.documentElement
 
+                image.classList.forEach(function(name) {
+                    if (name !== 'svg') {
+                        svg.classList.add(name)
+                    }
+                })
+
+                //svg.classList.add('fade-in')
+                image.parentElement.replaceChild(svg, image)
+            })
+            xhr.send()
+        }
+    })
 })();
