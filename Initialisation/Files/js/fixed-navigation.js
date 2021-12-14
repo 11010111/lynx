fd51633d3b7f3a3e486ab416109c8f30b78d92ea
Konstantin Schneider <k.schneider@s-w-e.com>
2021-12-14 10:15:30 +0100

    [update] base websiteTitle, fix on fixed-navigation.js

--- Initialisation/Files/js/fixed-navigation.js
+++ Initialisation/Files/js/fixed-navigation.js
@@ -1,34 +1,49 @@
 /**
+ * Desktop
  * Set Fixed Navigation On Scroll
  */
-(function FixedNavigationOnScroll() {
-
+(function FixedNavigationOnScrollDesktop() {
     let desktop = document.querySelector('.desktop')
+
+    if (!desktop) return
+
+    let navigation = desktop.querySelector('.desktop-container')
+
+    if (!navigation) return;
+
+    let desktopHeight = navigation.clientHeight
+
+    let scroll = function() {
+        if (window.scrollY > desktopHeight) {
+            desktop.classList.add('desktop-fixed')
+        } else {
+            desktop.classList.remove('desktop-fixed')
+        }
+    }
+
+    scroll()
+    window.addEventListener('scroll', scroll)
+})();
+
+/**
+ * Mobile
+ * Set Fixed Navigation On Scroll
+ */
+(function FixedNavigationOnScrollMobile() {
     let mobile = document.querySelector('.mobile')
 
-    if (desktop) {
-        let navigation = desktop.querySelector('.desktop-container')
-        let desktopHeight = navigation.clientHeight * 0.75
+    if (!mobile) return
 
-        window.addEventListener('scroll', function() {
-            if (window.scrollY > desktopHeight) {
-                desktop.classList.add('desktop-fixed')
-            } else {
-                desktop.classList.remove('desktop-fixed')
-            }
-        })
+    let mobileHeight = mobile.clientHeight
+
+    let scroll = function() {
+        if (window.scrollY > mobileHeight) {
+            mobile.classList.add('mobile-fixed')
+        } else {
+            mobile.classList.remove('mobile-fixed')
+        }
     }
 
-    if (mobile) {
-        let mobileHeight = mobile.clientHeight
-
-        window.addEventListener('scroll', function() {
-            if (window.scrollY > mobileHeight) {
-                mobile.classList.add('mobile-fixed')
-            } else {
-                mobile.classList.remove('mobile-fixed')
-            }
-        })
-    }
-
+    scroll()
+    window.addEventListener('scroll', scroll)
 })();
