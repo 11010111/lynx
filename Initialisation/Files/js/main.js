9db763a1a847c38927c2c8000cfea7f3a07d2dba
Konstantin Schneider <k.schneider@s-w-e.com>
2022-01-09 09:27:03 +0100

    [update] add fluid assets

--- Initialisation/Files/js/main.js
+++ Initialisation/Files/js/main.js
@@ -1,3 +1,1 @@
 console.log('WE LOVE TYPO3')
-
-const tobii = new Tobii()
---
+++ Initialisation/Files/js/slider.js
@@ -0,0 +1,26 @@
+/**
+ * Base Initialisation for Container Slider
+ */
+import Swiper from 'https://unpkg.com/swiper@7/swiper-bundle.esm.browser.min.js'
+
+const swiperContainer = new Swiper('.slider-content', {
+    // Optional parameters
+    loop: true,
+    slideClass: 'slider-cell',
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
