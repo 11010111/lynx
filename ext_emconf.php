395c38d6f42ae06f3944262d190d830523f5ffef
Konstantin Schneider <k.schneider@s-w-e.com>
2022-01-10 14:34:02 +0100

    [update] release 11.5.13

--- ext_emconf.php
+++ ext_emconf.php
@@ -30,5 +30,5 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Konstantin Schneider',
     'author_email' => 'k.schneider@s-w-e.com',
     'author_company' => 'Smart Web Elements',
-    'version' => '11.5.12',
+    'version' => '11.5.13',
 ];
