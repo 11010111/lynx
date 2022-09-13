bb57bc89df1a6e3f86f77241d2b59cf367904a5d
Konstantin Schneider <schneider.konstantin@outlook.com>
2022-09-13 11:17:08 +0200

    [update] browser scheme detecting

--- Initialisation/Files/js/main.js
+++ Initialisation/Files/js/main.js
@@ -1,1 +1,6 @@
+// Detect Browser Dark Scheme
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.body.classList.add('dark-scheme')
+}
+
 console.log('WE LOVE TYPO3')
