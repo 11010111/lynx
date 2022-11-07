e9c2dc04589316c2b148150d76ee81b1267520ab
Konstantin Schneider <k.schneider@s-w-e.com>
2022-11-07 16:22:02 +0100

    [update] AfterPackageActivationEvent

--- Classes/EventListener/LynxInitialisation.php
+++ Classes/EventListener/LynxInitialisation.php
@@ -10,7 +10,7 @@ use TYPO3\CMS\Core\Resource\Exception\InsufficientFolderAccessPermissionsExcepti
 use TYPO3\CMS\Core\Resource\Exception\InsufficientFolderWritePermissionsException;
 use TYPO3\CMS\Core\Resource\ResourceFactory;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Extensionmanager\Event\AfterExtensionFilesHaveBeenImportedEvent;
+use TYPO3\CMS\Core\Package\Event\AfterPackageActivationEvent;
 
 /**
  * Class LynxInitialisation
@@ -20,12 +20,12 @@ use TYPO3\CMS\Extensionmanager\Event\AfterExtensionFilesHaveBeenImportedEvent;
 class LynxInitialisation
 {
     /**
-     * @param AfterExtensionFilesHaveBeenImportedEvent $event
+     * @param AfterPackageActivationEvent $event
      * @throws ExistingTargetFolderException
      * @throws InsufficientFolderAccessPermissionsException
      * @throws InsufficientFolderWritePermissionsException
      */
-    public function __invoke(AfterExtensionFilesHaveBeenImportedEvent $event)
+    public function __invoke(AfterPackageActivationEvent $event)
     {
         if ($event->getPackageKey() === 'lynx') {
             /** @var ResourceFactory $resourceFactory */
