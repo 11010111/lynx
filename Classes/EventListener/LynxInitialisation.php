2246687d982009f8a249afab41a67bf29bc017f6
Konstantin Schneider <k.schneider@s-w-e.com>
2023-07-28 11:53:48 +0200

    [update] packages, margins, containers

--- Classes/EventListener/LynxInitialisation.php
+++ Classes/EventListener/LynxInitialisation.php
@@ -10,7 +10,7 @@ use TYPO3\CMS\Core\Resource\Exception\InsufficientFolderAccessPermissionsExcepti
 use TYPO3\CMS\Core\Resource\Exception\InsufficientFolderWritePermissionsException;
 use TYPO3\CMS\Core\Resource\ResourceFactory;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Extensionmanager\Event\AfterExtensionDatabaseContentHasBeenImportedEvent;
+use TYPO3\CMS\Extensionmanager\Event\AfterExtensionFilesHaveBeenImportedEvent;
 
 /**
  * Class LynxInitialisation
@@ -20,12 +20,12 @@ use TYPO3\CMS\Extensionmanager\Event\AfterExtensionDatabaseContentHasBeenImporte
 class LynxInitialisation
 {
     /**
-     * @param AfterExtensionDatabaseContentHasBeenImportedEvent $event
+     * @param AfterExtensionFilesHaveBeenImportedEvent $event
      * @throws ExistingTargetFolderException
      * @throws InsufficientFolderAccessPermissionsException
      * @throws InsufficientFolderWritePermissionsException
      */
-    public function __invoke(AfterExtensionDatabaseContentHasBeenImportedEvent $event)
+    public function __invoke(AfterExtensionFilesHaveBeenImportedEvent $event)
     {
         if ($event->getPackageKey() === 'lynx') {
             /** @var ResourceFactory $resourceFactory */
--- Resources/Public/JavaScript/Src/slider.js
+++ Resources/Public/JavaScript/Src/slider.js
@@ -1,4 +1,5 @@
-import Swiper, { Navigation, Pagination } from 'swiper'
+import Swiper from 'swiper'
+import { Navigation, Pagination } from 'swiper/modules'
 
 import 'swiper/css'
 import 'swiper/css/navigation'
