5de328020101a0c24335a28d2238e1d3d101ca65
Luca Braun <l.braun@s-w-e.com>
2023-11-03 15:53:48 +0100

    Outsourced sass compiling into dart-sass console command, implemented code splitting for js

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -2,42 +2,33 @@ import path from 'path'
 import esbuild from 'esbuild'
 import * as glob from 'glob'
 import * as fsExtra from 'fs-extra'
-import { sassPlugin } from 'esbuild-sass-plugin'
 
 const watch = process.argv.includes('--watch')
-const outDir = 'Resources/Public'
+const outDir = 'Resources/Public/JavaScript/Dist'
+const inDir = 'Resources/Public/JavaScript/Src'
+const extensions = ['js', 'ts']
 
-let entryPoints = []
+const entryPoints = setEntryPoints()
 
-const paths = [
-  {
-    directory: path.join('Resources', 'Public', 'Scss'),
-    out: path.join('Css'),
-    ext: ['scss', 'sass'],
-  },
-  {
-    directory: path.join('Resources', 'Public', 'JavaScript', 'Src'),
-    out: path.join('JavaScript', 'Dist'),
-    ext: ['js', 'ts'],
-  },
-]
-
-paths.forEach(type => {
-  const dir = `${type.directory}/**/*.{${type.ext.join(',')}}`
+function setEntryPoints() {
+  let entryPoints = []
+  const dir = `${inDir}/**/*.{${extensions.join(',')}}`
 
   glob.sync(dir).forEach(file => {
     if (path.parse(file).name.startsWith('_') || path.parse(file).base.includes('.d.ts')) {
       return
     }
 
-    const fullOutPath = path.parse(file).dir.replace(type.directory, type.out)
+    const fullOutPath = path.parse(file).dir === inDir ? '' : path.parse(file).dir.replace(inDir + '/', '')
 
     entryPoints.push({
       out: path.join(fullOutPath, path.parse(file).name),
       in: file
     })
   })
-})
+
+  return entryPoints
+}
 
 const notificationPlugin = {
   name: 'notification-plugin',
@@ -61,9 +52,9 @@ const options = {
   sourcemap: true,
   splitting: true,
   format: 'esm',
-  external: ['../Icons*', '../Font*'],
+  // external: ['../Icons*', '../Font*'],
   plugins: [
-    sassPlugin(),
+    // sassPlugin(),
     notificationPlugin,
   ]
 }
@@ -75,18 +66,16 @@ const productionOptions = {
   treeShaking: true
 }
 
+fsExtra.emptyDirSync(outDir)
+
 if (watch) {
   const ctx = await esbuild.context(options)
   await ctx.watch()
-  console.log('[esbuild]: Watching...')
+  console.log('[esbuild] Watching...')
 } else {
-  paths.forEach(type => {
-    fsExtra.emptyDirSync(path.join(outDir, type.out))
-  })
-
   await esbuild.build({
     ...options,
     ...productionOptions,
   })
-  console.log('[esbuild]: Build complete')
+  console.log('[esbuild] Build complete')
 }
